d268a1bd23b7b724324686dadd2f87bf2f377d9d
PhilipWitte <witte2008@gmail.com>
2015-02-09 21:16:03 -0800

    made Nim code more idiomatic

--- Nim/raytrace.nim
+++ Nim/raytrace.nim
@@ -6,8 +6,8 @@ when defined(bigImgMT):
 
 # ---
 
-const depthMax = 6
 const fov = 45.0
+const maxDepth = 6
 
 when defined(avgRuns):
   const runCount = 20
@@ -24,9 +24,7 @@ else:
 
 # ---------- ---------- ---------- #
 
-type Vec3 =
-  tuple
-    x, y, z: float
+type Vec3 = tuple[x, y, z:float]
 
 # ---
 
@@ -45,38 +43,38 @@ proc `*`(a:Vec3, b:float): Vec3 = (a.x * b, a.y * b, a.z * b)
 proc `/`(a, b:Vec3): Vec3 = (a.x / b.x, a.y / b.y, a.z / b.z)
 proc `/`(a:Vec3, b:float): Vec3 = (a.x / b, a.y / b, a.z / b)
 
-proc `+=`(this:var Vec3, v:Vec3) =
-  this.x += v.x
-  this.y += v.y
-  this.z += v.z
+proc `+=`(a:var Vec3, b:Vec3) =
+  a.x += b.x
+  a.y += b.y
+  a.z += b.z
 
 proc dot(a, b:Vec3): float = (a.x * b.x) + (a.y * b.y) + (a.z * b.z)
 proc normalize(v:Vec3): Vec3 = v / sqrt(dot(v, v))
 
-converter toVec3[X, Y, Z:SomeNumber](t:tuple[x:X, y:Y, z:Z]): Vec3 =
-  (float(t.x), float(t.y), float(t.z))
+converter toVec3[T:SomeNumber](v:tuple[x:T, y:T, z:T]): Vec3 =
+  (float(v.x), float(v.y), float(v.z))
 
 {.pop.}
 
 # ---------- ---------- ---------- #
 
-type Ray =
-  object
-    pos, dir: Vec3
-
-type Sphere =
-  ref object
+type
+  Ray = object
+    pos: Vec3
+    dir: Vec3
+  
+  Light = ref object
+    pos: Vec3
+    color: Vec3
+  
+  Sphere = ref object
     pos: Vec3
     color: Vec3
     radius: float
-    refl, tran: float
-
-type Light =
-  ref object
-    pos, color: Vec3
-
-type Scene =
-  ref object
+    refl: float
+    tran: float
+  
+  Scene = ref object
     objects: seq[Sphere]
     lights: seq[Light]
 
@@ -91,38 +89,38 @@ proc new(T:type Scene): Scene =
 
 {.push noInit.}
 
-proc normal(this:Sphere, pos:Vec3): Vec3 {.inline.} =
-  return normalize(pos - this.pos)
+proc normal(sphere:Sphere, pos:Vec3): Vec3 {.inline.} =
+  return normalize(pos - sphere.pos)
 
 
-proc intersect(this:Sphere, ray:Ray): bool =
-  let d = this.pos - ray.pos
+proc intersect(sphere:Sphere, ray:Ray): bool =
+  let d = sphere.pos - ray.pos
   let a = dot(d, ray.dir)
   if a < 0: # opposite direction
     return false
 
   let b2 = dot(d, d) - (a * a)
-  let r2 = this.radius * this.radius
+  let r2 = sphere.radius * sphere.radius
   if b2 > r2: # perpendicular > r
     return false
   
   return true
 
 
-proc intersect(this:Sphere, ray:Ray, distance:var float): bool =
+proc intersect(sphere:Sphere, ray:Ray, distance:var float): bool =
   distance = 0
   
-  let d = this.pos - ray.pos
+  let d = sphere.pos - ray.pos
   let a = dot(d, ray.dir)
   if a < 0: # opposite direction
     return false
   
-  let b2 = dot(d, d) - (a * a)
-  let r2 = this.radius * this.radius
-  if b2 > r2: # perpendicular > r
+  let b = dot(d, d) - (a * a)
+  let r = sphere.radius * sphere.radius
+  if b > r: # perpendicular > r
     return false
   
-  let c = sqrt(r2 - b2)
+  let c = sqrt(r - b)
   let near = a - c
   let far  = a + c
   # near < 0 means ray starts inside
@@ -131,14 +129,14 @@ proc intersect(this:Sphere, ray:Ray, distance:var float): bool =
   return true
 
 
-proc trace(this:Ray, scene:Scene, depth:int): Vec3 = 
+proc trace(ray:Ray, scene:Scene, depth:int): Vec3 = 
   var nearest = Inf
   var obj: Sphere
   
   # search the scene for nearest intersection
   for o in scene.objects:
     var distance = Inf
-    if o.intersect(this, distance):
+    if o.intersect(ray, distance):
       if distance < nearest:
         nearest = distance
         obj = o
@@ -146,11 +144,11 @@ proc trace(this:Ray, scene:Scene, depth:int): Vec3 =
   if obj == nil:
     return Vec3.zero
   
-  let hitPoint = this.pos + (this.dir * nearest)
+  let hitPoint = ray.pos + (ray.dir * nearest)
   var normal = obj.normal(hitPoint)
   var inside = false
   
-  if dot(normal, this.dir) > 0:
+  if dot(normal, ray.dir) > 0:
     inside = true
     normal = -normal
   
@@ -172,23 +170,23 @@ proc trace(this:Ray, scene:Scene, depth:int): Vec3 =
       color += light.color * max(0, dot(normal, lightDir)) *
         obj.color * (1 - reflRatio)
   
-  let rayNormDot = dot(this.dir, normal)
+  let rayNormDot = dot(ray.dir, normal)
   let facing = max(0, -rayNormDot)
   let fresnel = reflRatio + ((1 - reflRatio) * pow((1 - facing), 5))
   
   # compute reflection
-  if depth < depthMax and reflRatio > 0:
-    let reflDir = this.dir + (normal * 2 * rayNormDot * -1)
+  if depth < maxDepth and reflRatio > 0:
+    let reflDir = ray.dir + (normal * 2 * rayNormDot * -1)
     let tr = Ray(pos:hitPoint + (normal * 1.0e-5), dir:reflDir)
     color += tr.trace(scene, depth + 1) * fresnel
   
   # compute refraction
-  if depth < depthMax and obj.tran > 0:
+  if depth < maxDepth and obj.tran > 0:
     const iorDefault = 1 / 1.5
     let ior = if inside: iorDefault else: 1.5
-    let ce = dot(this.dir, normal) * -1
+    let ce = dot(ray.dir, normal) * -1
     let eta = 1 / ior
-    let gf = (this.dir + normal * ce) * eta
+    let gf = (ray.dir + normal * ce) * eta
     let sin_t1_2 = 1 - (ce * ce)
     let sin_t2_2 = sin_t1_2 * (eta * eta)
     
@@ -209,7 +207,7 @@ type Pixmap = array[pixmapSize, byte]
 
 # ---
 
-proc renderRegion(this:ref Pixmap, scene:Scene, w, h:float, sx, sy, ex, ey:int) =
+proc renderRegion(pixmap:ref Pixmap, scene:Scene, w, h:float, sx, sy, ex, ey:int) =
   let wf: float = width
   let hf: float = height
   
@@ -225,17 +223,17 @@ proc renderRegion(this:ref Pixmap, scene:Scene, w, h:float, sx, sy, ex, ey:int) 
       let pixel = ray.trace(scene, 0)
       let index = (x * 3) + (y * width * 3)
       
-      this[index]   = byte min(pixel.x * 255, 255)
-      this[index+1] = byte min(pixel.y * 255, 255)
-      this[index+2] = byte min(pixel.z * 255, 255)
+      pixmap[index]   = byte min(pixel.x * 255, 255)
+      pixmap[index+1] = byte min(pixel.y * 255, 255)
+      pixmap[index+2] = byte min(pixel.z * 255, 255)
 
 
-proc render(this:ref Pixmap, scene:Scene) =
+proc render(pixmap:ref Pixmap, scene:Scene) =
   let h = tan(((fov / 360) * (2 * Pi)) / 2) * 2
   let w = h * width / height
   
   when not defined(bigImgMT):
-    renderRegion(this, scene, w, h, 0, 0, width-1, height-1)
+    renderRegion(pixmap, scene, w, h, 0, 0, width-1, height-1)
   else:
     let sizeW = int(width / tiles)
     let sizeH = int(height / tiles)
@@ -246,7 +244,7 @@ proc render(this:ref Pixmap, scene:Scene) =
           let sy = y * sizeH
           let ex = sx + sizeW - 1
           let ey = sy + sizeH - 1
-          spawn renderRegion(this, scene, w, h, sx, sy, ex, ey)
+          spawn renderRegion(pixmap, scene, w, h, sx, sy, ex, ey)
 
 # ---------- ---------- ---------- #
 
@@ -256,11 +254,11 @@ proc main =
   let image = new Pixmap
   
   # add geometry
-  scene.objects.add Sphere(pos:(0, -10002, -20), radius:10000, color:(0.8, 0.8, 0.8))
-  scene.objects.add Sphere(pos:( 0,  2, -20), radius:4, color:(0.8, 0.5, 0.5), refl:0.5)
-  scene.objects.add Sphere(pos:( 5,  0, -15), radius:2, color:(0.3, 0.8, 0.8), refl:0.2)
-  scene.objects.add Sphere(pos:(-5,  0, -15), radius:2, color:(0.3, 0.5, 0.8), refl:0.2)
-  scene.objects.add Sphere(pos:(-2, -1, -10), radius:1, color:(0.1, 0.1, 0.1), refl:0.1, tran:0.8)
+  scene.objects.add Sphere(pos:(0, -10002, -20), color:(0.8, 0.8, 0.8), radius:10000)
+  scene.objects.add Sphere(pos:( 0,  2, -20), color:(0.8, 0.5, 0.5), radius:4, refl:0.5)
+  scene.objects.add Sphere(pos:( 5,  0, -15), color:(0.3, 0.8, 0.8), radius:2, refl:0.2)
+  scene.objects.add Sphere(pos:(-5,  0, -15), color:(0.3, 0.5, 0.8), radius:2, refl:0.2)
+  scene.objects.add Sphere(pos:(-2, -1, -10), color:(0.1, 0.1, 0.1), radius:1, refl:0.1, tran:0.8)
   
   # add light
   scene.lights.add Light(pos:(-10, 20, 30), color:(2, 2, 2))
